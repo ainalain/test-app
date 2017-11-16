a2c0f6529cead506134addf2b4c65407e17b670e
Elena Sufieva <fiolent06@gmail.com>
2017-11-16 18:49:57 +0300

    Make random authors for comments
    In our app user isn't logged in, so we have to make a name for her.

--- src/api/mockCommentsApi.js
+++ src/api/mockCommentsApi.js
@@ -4,17 +4,43 @@ import comments from './comments.json';
 
 const delay = 1000;
 
-const defaultAuthor = 'William Shakespeare';
+const authors = [
+  'Jane Austen',
+  'William Blake',
+  'Geoffrey Chaucer',
+  'Charles Dickens 1812-1870',
+  'John Donne',
+  'George Eliot',
+  'William Shakespeare',
+  'John Milton',
+  'George Orwell',
+  'Emily Dickinson',
+  'William Faulkner',
+  'F. Scott Fitzgerald',
+  'Robert Frost',
+  'Ernest Hemingway',
+];
+
+const getRandomAuthor = () => {
+  const max = authors.length - 1;
+  const min = 0;
+  const index = Math.floor(Math.random()*(max-min+1)+min);
+  return authors[index];
+};
 
 const makeCommentProps = () => {
   const currentDate = moment();
   const date = moment(currentDate).format("Do MMMM YYYY, h:mm a");
   const id = Math.round(+new Date()/1000);
+
+  const author = getRandomAuthor();
   return {
     date,
     id,
+    author,
   };
 };
+
 
 // This file mocks a web API by working with the hard-coded data below.
 // It uses setTimeout to simulate the delay of an AJAX call.
