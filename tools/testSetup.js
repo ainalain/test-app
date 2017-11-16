b561af902d21b7bfb8a59fbdfeded8c1bd16d1dc
Elena Sufieva <fiolent06@gmail.com>
2017-11-16 13:45:22 +0300

    Update enzyme to 3.2.0

--- tools/testSetup.js
+++ tools/testSetup.js
@@ -1,11 +1,11 @@
 process.env.NODE_ENV = 'test';
 
-var sinon = require('sinon');
+const sinon = require('sinon');
 
 require('babel-register')();
 
-var hook = require('css-modules-require-hook');
-var sass = require('node-sass');
+const hook = require('css-modules-require-hook');
+const sass = require('node-sass');
 
 hook({
   extensions: [ '.scss', '.css' ],
@@ -20,8 +20,16 @@ require.extensions['.svg'] = function (module, filename) {
    module.exports = fs.readFileSync(filename, 'utf8');
 };
 
+
 global.sinon = sinon;
 
 global.navigator = {
   userAgent: 'node.js'
 };
+
+const Adapter = require('enzyme-adapter-react-16');
+const enzyme = require('enzyme');
+
+enzyme.configure({ adapter: new Adapter() });
+
+module.exports = enzyme;  
