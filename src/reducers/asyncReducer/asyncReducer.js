763f249efe24e8009ccd9f3aa8bee4459fb33479
Elena Sufieva <fiolent06@gmail.com>
2017-11-19 14:48:14 +0300

    Change async handling, remove async actions

--- src/reducers/asyncReducer/asyncReducer.js
+++ src/reducers/asyncReducer/asyncReducer.js
@@ -1,36 +1,40 @@
 import {
-  BEGIN_AJAX_CALL,
-  AJAX_CALL_ERROR,
-  LOAD_COMMENTS_SUCCESS,
+  GET_PRODUCT,
+  GET_PRODUCT_FAILURE,
   GET_PRODUCT_SUCCESS,
+  GET_ALL_COMMENTS,
+  GET_ALL_COMMENTS_FAILURE,
+  GET_ALL_COMMENTS_SUCCESS,
+  ADD_COMMENT,
+  ADD_COMMENT_FAILURE,
   ADD_COMMENT_SUCCESS,
+  CLEAR_DEFAULT_COMMENTS,
+  CLEAR_DEFAULT_COMMENTS_FAILURE,
+  CLEAR_DEFAULT_COMMENTS_SUCCESS
 } from '../../actions/actionTypes';
 
 import { isLoading } from '../initialState';
 
-const successActions = [
-  LOAD_COMMENTS_SUCCESS,
-  GET_PRODUCT_SUCCESS,
-  ADD_COMMENT_SUCCESS,
-];
-
 export default function asyncReducer(state = isLoading, { type, payload }) {
   switch (type) {
-    case BEGIN_AJAX_CALL:
-      if (payload === 'comments') {
-        return { ...state, comments: 1 };
-      }
+    case GET_PRODUCT:
       return { ...state, product: 1 };
-    case LOAD_COMMENTS_SUCCESS:
+    case GET_ALL_COMMENTS:
+    case ADD_COMMENT:
+    case CLEAR_DEFAULT_COMMENTS:
+      return { ...state, comments: 1 };
+    case GET_ALL_COMMENTS_SUCCESS:
     case ADD_COMMENT_SUCCESS:
+    case CLEAR_DEFAULT_COMMENTS_SUCCESS:
       return { ...state, comments: 0 };
     case GET_PRODUCT_SUCCESS:
       return { ...state, product: 0 };
-    case AJAX_CALL_ERROR:
-      if (payload.target === 'comments') {
-        return { ...state, comments: 0, error: payload.eror };
-      }
+    case GET_PRODUCT_FAILURE:
       return { ...state, product: 0, error: payload.error };
+    case GET_ALL_COMMENTS_FAILURE:
+    case ADD_COMMENT_FAILURE:
+    case CLEAR_DEFAULT_COMMENTS_FAILURE:
+    return { ...state, comments: 0, error: payload.error };
     default:
       return state;
   }
